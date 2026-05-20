7e3d431dfd58c33b5715e2fc0a1433a98ecdc512
Bogdan <bogdan.zlatovcen@gmail.com>
2026-05-20 18:44:37 +0300

    delete local run methods

--- lab1/code/openmp/compile.sh
+++ lab1/code/openmp/compile.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-if [[ "$(uname)" == "Darwin" ]]; then
-  LIBOMP="$(brew --prefix libomp 2>/dev/null || true)"
-  if [[ -z "$LIBOMP" ]]; then
-    echo "On macOS install OpenMP first: brew install libomp"
-    exit 1
-  fi
-  g++ -O3 -Xpreprocessor -fopenmp -I"$LIBOMP/include" -L"$LIBOMP/lib" -lomp \
-    lab1_openmp.cpp sha256.cpp -o lab1_openmp
-else
-  g++ -O3 -fopenmp lab1_openmp.cpp sha256.cpp -o lab1_openmp
-fi
+g++ -O3 -fopenmp lab1_openmp.cpp sha256.cpp -o lab1_openmp
